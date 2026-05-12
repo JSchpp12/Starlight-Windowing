3558c4c5701210a7c4dd3ff8c029c4f5b4d36207
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-05-12 07:40:02 -0400

    QUICK: Add builder for camera

--- include/star_windowing/BasicCamera.hpp
+++ include/star_windowing/BasicCamera.hpp
@@ -24,6 +24,78 @@ class BasicCamera : public StarCamera,
                     private HandleKeyReleasePolicy<BasicCamera>
 {
   public:
+    class Builder
+    {
+        float m_horzFov{0.0f};
+        float m_nearClippingPlane{0.0f};
+        float m_farClippingPlane{0.0f};
+        float m_movementSpeed{0.0f};
+        float m_sensitivity{0.0f};
+        uint32_t m_width{0};
+        uint32_t m_height{0};
+
+      public:
+        Builder() = default;
+        Builder &setWidth(uint32_t width)
+        {
+            m_width = std::move(width);
+            return *this;
+        }
+        Builder &setHeight(uint32_t height)
+        {
+            m_height = std::move(height);
+            return *this;
+        }
+        Builder &setHorizontalFieldOfView(float value)
+        {
+            m_horzFov = std::move(value);
+            return *this;
+        }
+        Builder &setNearClippingPlaneDistance(float value)
+        {
+            m_nearClippingPlane = std::move(value);
+            return *this;
+        }
+        Builder &setFarClippingPlaneDistance(float value)
+        {
+            m_farClippingPlane = std::move(value);
+            return *this;
+        }
+        Builder &setMovementSpeed(float value)
+        {
+            m_movementSpeed = std::move(value);
+            return *this;
+        }
+        Builder &setSensitivity(float value)
+        {
+            m_sensitivity = std::move(value);
+            return *this;
+        }
+        std::unique_ptr<star::StarCamera> buildUnique()
+        {
+            if (m_horzFov != 0.0f)
+            {
+                return std::make_unique<BasicCamera>(m_width, m_height, m_horzFov, m_nearClippingPlane,
+                                                     m_farClippingPlane, m_movementSpeed, m_sensitivity);
+            }
+            else
+            {
+                return std::make_unique<BasicCamera>(m_width, m_height);
+            }
+        }
+        std::shared_ptr<BasicCamera> buildShared()
+        {
+            if (m_horzFov != 0.0f)
+            {
+                return std::make_shared<BasicCamera>(m_width, m_height, m_horzFov, m_nearClippingPlane,
+                                                     m_farClippingPlane, m_movementSpeed, m_sensitivity);
+            }
+            else
+            {
+                return std::make_shared<BasicCamera>(m_width, m_height);
+            }
+        }
+    };
     BasicCamera();
     BasicCamera(const uint32_t &width, const uint32_t &height);
     BasicCamera(const uint32_t &width, const uint32_t &height, const float &horizontalFieldOfView,
